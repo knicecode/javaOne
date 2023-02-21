978634c8641a09915ad8f3f6e9bc82b2e1caeda0
knicecode <carterknicelyde@gmail.com>
2023-02-21 00:13:27 -0500

    Working console version that prompts the user for input, shows both selections, and determines a winner.

---
+++ allFiles/javascript.js
@@ -0,0 +1,94 @@
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+
+function getRandomNum(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function getRandomChoice() {
+  const randomNum = getRandomNum(choices.length);
+  return choices[randomNum];
+}
+
+
+
+
+const playerSelection = getPlayerChoice()
+const computerSelection = getRandomChoice()
+let win = "You win!"
+let lose = "You lose!"
+let tie = "It's a tie!"
+let playerScore = 0
+let computerScore = 0
+
+function getPlayerChoice() {
+  let playerInput = prompt("Rock, paper, or scissors?")
+  let response = ""
+  
+  if (playerInput = "Rock") {
+    response = choices[0]
+  } else if (playerInput = "Paper") {
+    response = choices[1]
+  } else if (playerInput = "Scissors") {
+    response = choices[2]
+  }
+
+  return response
+}
+
+
+function playRound(playerSelection, computerSelection) {
+  
+  let roundResult = ""
+
+  if (playerSelection == choices[0]) {
+    if (computerSelection == choices[0]) {
+      roundResult = tie
+    } else if (computerSelection == choices[1]) {
+      roundResult = lose
+    } else {
+      roundResult = win
+    }
+  
+  } else if (playerSelection == choices[1]) {
+    if (computerSelection == choices[0]) {
+      roundResult = win
+    } else if (computerSelection == choices[1]) {
+      roundResult = tie
+    } else {
+      roundResult = lose
+    }
+  
+  } else if (playerSelection == choices[2]) {
+    if (computerSelection == choices[0]) {
+      roundResult = lose
+    } else if (computerSelection == choices[1]) {
+      roundResult = win
+    } else {
+      roundResult = tie
+    }
+  } 
+  
+
+  
+  return roundResult
+}
+
+function game() {
+  for (i = 0; i < 5; i++) {
+    if (roundResult = win) {
+      playerScore++
+    
+    } else if (roundResult = lose) {
+      computerScore++
+    
+    } else if (roundResult = tie) {
+      i--
+    }
+  }
+}
+
+
+console.log(playRound(playerSelection, computerSelection))
+
+console.log("Computer choice:", computerSelection)
+console.log("My choice:", playerSelection)
